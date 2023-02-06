ac149c85f9caf87c781ca2ffd1836b8a961b989a
vse-opensource <virtualspace.suporte@gmail.com>
2023-02-06 00:25:55 -0300

    deploy

---
+++ gmaps.php
@@ -0,0 +1,5 @@
+<div class="titles has-text-centered">
+    <h1 class="title is-1 fine">Como Chegar</h1>
+</div>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.5593180530122!2d-46.683488785214635!3d-23.584265668323262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5742bef173a9%3A0x3888a2ae082f2bd7!2sRua%20Pais%20de%20Ara%C3%BAjo%2C%2029%20-%20Itaim%20Bibi%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2004531-090!5e0!3m2!1sen!2sbr!4v1603726384239!5m2!1sen!2sbr" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
